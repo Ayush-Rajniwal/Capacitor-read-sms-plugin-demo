3170e6e75c2a9f7676e285a012ba677630bd2796
Ayush Rajniwal <ayush.rajniwal@gmail.com>
2021-10-10 17:29:55 +0530

    🌻 Example of Read-SMS Capacitor Plugin

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,61 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
 import './Home.css';
 
+import { Plugins } from "@capacitor/core";
+import "read-sms";
+const { ReadSMS } = Plugins;
+
 const Home: React.FC = () => {
+
+  const [msg, setMsg] = useState<any>([]);
+  const [permStatus, setPermStatus] = useState("");
+	const readSMS = async () => {
+		const sms = (await ReadSMS.getSMS()).value;
+		setMsg((val:any) => [...val, ...sms]);
+		console.log(sms, "value of SMS");
+	}
+
+	const requestPermission = async () => {
+		const permission = (await ReadSMS.requestPermission()).value;
+    setPermStatus(permission);
+		console.log(permission, "permission");
+	}
+
+	const checkPermission = async () => {
+		const permission = (await ReadSMS.checkPermission()).value;
+    setPermStatus(permission);
+		console.log(permission, "checkPermission");
+	}
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Read SMS Example</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Read SMS Example</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+          <IonButton onClick={()=>readSMS()}>Read SMS</IonButton>
+          <IonButton onClick={()=>requestPermission()}>Requst SMS Permission</IonButton>
+          <IonButton onClick={()=>checkPermission()}>Check current status of SMS Permission</IonButton>
+    <div>
+      {permStatus && <p>Permission: {permStatus}</p>}
+      {msg.map((m:any) =>
+        <IonCard>
+          <IonCardHeader>
+            {m.address}
+          </IonCardHeader>
+          <IonCardContent>
+            {m.body}
+          </IonCardContent>
+        </IonCard>)
+      }
+    </div>
       </IonContent>
     </IonPage>
   );
